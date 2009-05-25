1e3e3ce89b153a27011da64c73c5291dd12888a0
phil <phil@gurke.(none)>
2009-05-25 12:20:45 +0200

    fixed result-collection bug

--- lib/xmms_adapter.rb
+++ lib/xmms_adapter.rb
@@ -21,28 +21,23 @@ class XmmsAdapter
   # generate a idlist-collection from path, returns name
   def new_idlist_from_path(path, name=nil)
     ns = Xmms::Collection::NS_COLLECTIONS
-    puts "[DEBUG] importing #{path.inspect}"
     import(path)
     name ||= path.split(File::SEPARATOR).last
-    puts "[DEBUG] name will be #{name.inspect}"
     collection = Xmms::Collection.parse("url:*#{path}*")
     idlist = @xmms.coll_query_ids(collection).wait.value
-    puts "[DEBUG] idlist is #{idlist.inspect}"
     collection.idlist = idlist
     @xmms.coll_save(collection, name, ns).wait
-    puts "[DEBUG] idlist saved as #{name.inspect} in namepsace #{ns.inspect}"
     name
   end 
   
   # queue least often played randomly from collection
   def qloprfc(name)
-    collection = @xmms.coll_get(name).wait
-    p collection
-
-    # info = info(collection.idlist)
-    # leasttimes = (info.map { |i| i[:timesplayed] }).min
-    # candidates = info.select { |i| leasttimes < i[:timesplayed] }
-    # add(candidates[rand(candidates.size)][:id])
+    ns = Xmms::Collection::NS_COLLECTIONS
+    collection = @xmms.coll_get(name, ns).wait.value
+    info = info(collection.idlist)
+    leasttimes = (info.map { |i| i[:timesplayed] }).min
+    candidates = info.select { |i| leasttimes < i[:timesplayed] }
+    add(candidates[rand(candidates.size)][:id])
   end 
   
   # retrieve info for the next @limit@ entries in current playlist
@@ -65,7 +60,17 @@ class XmmsAdapter
   
   # get array of info hashes from medialib
   def info(idlist)
-    idlist.map { |id| @xmms.medialib_get_info(id).wait.value }
+    p idlist
+    getc
+    rv = idlist.map do |id|
+      begin
+        @xmms.medialib_get_info(id).wait.value
+      rescue
+        # something's wrong with the ids
+        puts "ups. corrupt id? #{id}"
+      end
+    end
+    rv.reject { |e| e==nil }
   end 
   
   # recursively add path to current playlist (we don't need that!)
@@ -92,14 +97,18 @@ end
 if $0 == __FILE__
   xmms = XmmsAdapter.new
   xmms.play
+  case ARGV[0]
+    when 'import_path'
+    name = xmms.new_idlist_from_path(ARGV[1])
+    when 'add_from'
+    xmms.qloprfc(ARGV[1])
+    xmms.list.each { |e| puts "#{e[:artist]} - #{e[:title]} (#{e[:url]})" }
+  end
   # p xmms.add('/media/My Passport/musik/1999 - v Muslimgauze/Bass Communion v Muslimgauze - 01 - One.mp3')
   # p xmms.skip
   # r = xmms.list5
   # p r.first
   # r.each { |e| puts "#{e[:artist]} - #{e[:title]} (#{e[:url]})" }
-  name = xmms.new_idlist_from_path('/home/phil/mp3/niffpage.greywool.com/files/Music/Enskog')
-  xmms.qloprfc(name)
-  xmms.list.each { |e| puts "#{e[:artist]} - #{e[:title]} (#{e[:url]})" }
   # p xmms.import('/home/phil/mp3/niffpage.greywool.com/files/Music/Winterscapes')
   # p xmms.playlists
   # p xmms.collections
