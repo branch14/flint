df0a4c2c787dd0411916cf0e45d917d4106f4d23
phil <phil@gurke.(none)>
2009-06-13 09:57:11 +0200

    mix_in_our_stuff moved to Utils

--- lib/utils.rb
+++ lib/utils.rb
@@ -16,10 +16,15 @@ module Utils
     end
 
     def source
-      return '/media/usb/mp3test/' unless Dir.glob('/media/usb/mp3test/**').empty? # testing
       return '/media/cdrom/' if cdrom?
       if path = usb?
-        return path
+        subpath = "hochzeit"
+        keypath = File.join(path, subpath)
+        if File.directory?(keypath)
+          return keypath
+        else
+          return path
+        end
       end
       return false
     end
@@ -35,6 +40,20 @@ module Utils
         return md[1]
       end
       false
+    end
+
+    def mix_in_our_stuff(xmms)
+      now = DateTime::now
+      teatime   = (now >= DateTime.new(2009, 6, 19, 12) && now <= DateTime.new(2009, 6, 19, 18))
+      reception = (now >= DateTime.new(2009, 6, 20, 16) && now <= DateTime.new(2009, 6, 20, 20))
+      donner    = (now >= DateTime.new(2009, 6, 20, 20) && now <= DateTime.new(2009, 6, 20, 23))
+      latenight = (now >= DateTime.new(2009, 6, 20, 23) && now <= DateTime.new(2009, 6, 21, 06))
+      coll = "Phil & Sophia"
+      coll = "Phil & Sophia (TT)" if teatime 
+      coll = "Phil & Sophia (Rc)" if reception
+      coll = "Phil & Sophia (Di)" if dinner
+      coll = "Phil & Sophia (LN)" if latenight
+      xmms.append_from_collection coll
     end
     
   end
